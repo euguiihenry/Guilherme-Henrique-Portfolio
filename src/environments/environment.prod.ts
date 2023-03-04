97ca0a532cb5b80050645fe4fe4d747159ccf23d
Henry <gui2012451@gmail.com>
2023-03-04 11:39:08 -0300

    build: updating API keys methods - Removing DocEnv import from environments.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,3 @@
-
 export const environment = {
     production: true,
     PROJECTS: process.env['PROJECTS'] || '',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,3 @@
-import { config } from 'dotenv';
-
-config();
-
 export const environment = {
     production: false,
     PROJECTS: process.env['PROJECTS'] || '',
