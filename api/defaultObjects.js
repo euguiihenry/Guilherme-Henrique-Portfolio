fe6f1c63925403cb28ab9b1372333f8f41f94dbd
Henry <gui2012451@gmail.com>
2023-05-01 19:14:17 -0300

    fix: fixing the serverless function and changing vercel.json file

--- api/defaultObjects.js
+++ api/defaultObjects.js
@@ -1,31 +1,20 @@
 const express = require('express');
-const http = require('http');
 const cors = require('cors');
 const fs = require('fs');
 
 const app = express();
-const server = http.createServer(app);
-const port = 3001;
-
 app.use(cors());
 
 app.get('/api/langue-objects', async (req, res) => {
   try{
-      fs.readFile('./models/langue.json', async (err, data) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-
-        const info = JSON.parse(data);
-        console.log("You have made a request to this server");
-        res.send(info);
-      });
-
+      const data = await fs.promises.readFile('./public/models/langue.json');
+      const info = JSON.parse(data);
+      console.log("You have made a request to this server");
+      res.send(info);
   } catch (error) {
       console.error(error);
       res.status(500).send('Error retrieving data');
   }
-})
+});
 
-server.listen(port);
+module.exports = app;
