7ff32f609d08be2cc465240f8951797fd1699150
Henry <gui2012451@gmail.com>
2023-05-01 12:12:27 -0300

    updating api files

--- api/getAbout.js
+++ api/getAbout.js
@@ -1,7 +1,10 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import fetch from 'node-fetch';
+import express from "express";
 
-export default async function getAbout(req: VercelRequest, res: VercelResponse) {
+const app = express();
+
+/* export default async function getAbout(req: VercelRequest, res: VercelResponse) {
   try {
     // Set CORS headers
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -13,10 +16,25 @@ export default async function getAbout(req: VercelRequest, res: VercelResponse) 
     const data = await response.json();
 
     // Send the JSON data back as a response to the client
-    /* res.setHeader('Content-Type', 'application/json'); */
+    // res.setHeader('Content-Type', 'application/json');
     res.status(200).json(data);
   } catch (error) {
     console.error(error);
     res.status(500).send('Error fetching JSON data');
   }
-}
+} */
+
+app.get('/', async (req, res) => {
+  try {
+      const response = await fetch('https://gitfront.io/r/euguiihenry/jmb1cDCfAn54/portfolio-database/raw/about-phrases-global/about.json');
+      const data = await response.json();
+      res.send(data);
+  } catch (error) {
+      console.error(error);
+      res.status(500).send('Error fetching data');
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
