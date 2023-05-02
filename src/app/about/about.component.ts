105414aa4221ef8be5a1f33bcc6b13ed40b31c1e
Henry <gui2012451@gmail.com>
2023-05-01 21:59:42 -0300

    build: implementing the about page skeleton

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ArrangeLanguageService } from '../mutual/services/arrangeLanguage/arrange-language.service';
 
 @Component({
   selector: 'app-about',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent {
+  public title: string = '';
+  constructor(/* private getAboutPhrases: GetAboutPhrasesService, */ private siteLanguageService: ArrangeLanguageService) { }
 
+  //phrases: AboutPhrase[] = [];
+
+  defineLangue() {
+    const langueStr = localStorage.getItem('langueObj');
+
+    if (langueStr) {
+      try {
+        const langueObj = JSON.parse(langueStr);
+        this.title = langueObj.about.title;
+      } catch (e) {
+        console.error('Failed to parse localStorage item: ', e);
+      }
+    }
+  }
+
+  ngOnInit(): void {
+    this.defineLangue();
+
+/*     this.getAboutPhrases.getAboutPhrases().subscribe((phrase: AboutPhrase) => {
+      const langue = this.siteLanguageService.getLanguage();
+      const phrases = phrase[langue!]
+      this.phrases = Object.values(phrases);
+    }); */
+  }
 }
