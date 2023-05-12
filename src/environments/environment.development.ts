91f5ca4ba37eddda34c4b994d0092adf188e93cc
Henry <gui2012451@gmail.com>
2023-05-11 22:50:31 -0300

    Fix: updating environment file to test preview in vercel

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  HTTP_LINK: 'http://',
+  HTTP_LINK: 'https://',
   ABOUT_LINK: "/api/get-about",
   PROJECTS_LINK: "/api/get-projects",
   LANGUE_OBJECT_LINK: "/api/langue-objects",
